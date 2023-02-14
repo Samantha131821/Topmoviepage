162b66cef7ce84f98e8b3e68544537b4bad3c7be
00rest <119826790+00rest@users.noreply.github.com>
2023-02-13 22:08:06 -0500

    info page 90% ready

---
+++ assets/js/info.js
@@ -0,0 +1,39 @@
+var movieId = localStorage.getItem('movieId');
+console.log(movieId);
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+fetch('https://imdb-api.com/en/API/Title/k_kf6u348l/'+movieId, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+        
+            document.getElementById('infoTitle').innerText = result.fullTitle;
+            document.getElementById('infoPlot').innerText = result.plot;
+            document.getElementById('infoImg').setAttribute('src', result.image);
+            document.getElementById('infoRating').innerText = result.imDbRatingVotes+' votes';
+            document.getElementById('infoIMDbRating').innerHTML = result.imDbRating;
+            document.getElementById('infoStars').innerHTML = 'Staring: '+result.stars;
+            
+            var fetchUrl = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=' + result.title + '&api-key=HEot1qFM7Q0uXHMz4GJN00KpM4PuyyKv'
+            var options = {
+              method: 'GET',
+              redirect: 'follow'
+            };
+            fetch(fetchUrl, options)
+              .then(response => response.json())
+              .then(data => {
+                console.log(data);
+                document.getElementById('infoNYtimes').innerHTML = data.results[0].summary_short;
+                console.log(data.results[0].link.url);
+                document.getElementById('infoLink').setAttribute('href', data.results[0].link.url);
+            
+        
+              })
+              .catch(error => console.log('error', error));
+
+    })
+    .catch(error => console.log('error', error));
